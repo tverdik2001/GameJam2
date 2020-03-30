6994df98fb5479132597383f9abaded6870b51f1
Angela Dai <zhjh.angela@gmail.com>
2020-03-30 12:11:56 -0400

    fixed scroll; reordered sprites

--- Black&White/Assets/Scripts/Player.cs
+++ Black&White/Assets/Scripts/Player.cs
@@ -3,31 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{   private bool isDead;
-    private Rigidbody2D rb2d;
-    public float upForce=200f;
-    public float sp = 1.5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        isDead=false;
-        rb2d=GetComponent<Rigidbody2D>();
-        
+{ private bool isDead;
+  private Rigidbody2D rb2d;
+  public float upForce=200f;
+  public float sp = 1.5f;
+  // Start is called before the first frame update
+  void Start() {
+    isDead=false;
+    rb2d=GetComponent<Rigidbody2D>();
+  }
+  // Update is called once per frame
+  void Update(){ 
+   transform.position += Vector3.left * GameControl.instance1.scrollSpeed* Time.deltaTime;
+   if (Input.GetKey(KeyCode.UpArrow)) {
+     transform.Translate(Vector3.up * 7 * Time.deltaTime, Space.World);
     }
-
-    // Update is called once per frame
-    void Update()
-    { 
-    //if(Input.anyKey){
-      if (Input.GetKey(KeyCode.UpArrow)) {
-       transform.Translate(Vector3.up * 7 * Time.deltaTime, Space.World);
-      }
-      if (Input.GetKey(KeyCode.DownArrow)) {
-        transform.position += Vector3.down * sp * Time.deltaTime;
-      }
-      
-     //}
+   if (Input.GetKey(KeyCode.DownArrow)) {
+     transform.position += Vector3.down * sp * Time.deltaTime;
     }
+  }
 
    
 }
