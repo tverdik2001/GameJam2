0dac60d44f154475fabbf634d46925996e03a116
Yegor <yehor.tverdokhlibov@gmail.com>
2020-04-08 15:40:14 +0300

    Level 1 created
    GUI:
    1. Menu ( play, options, how to play)
    2. If the player touches the borders or some obstacle ,, then --- NEW window GAME OVER +button start over.
    3.When the ball reaches the black cube this is EXIT . Create a new window that says LEVEL complete and two buttons go to the next level or HOME.

---
+++ GameJam2NEW/Assets/BlockControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockControl : MonoBehaviour
+{
+
+    public enum DirectionX { none, left, right}
+    public enum DirectionY { none, up, down}
+
+
+    public float speed = 0.2f;
+    public float offsetX = 1;
+    public float offsetY = 2;
+
+    public DirectionX directionX;
+    public DirectionY directionY;
+
+    float startX;
+    float startY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startX = transform.position.x;
+        startY = transform.position.y;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (directionY != DirectionY.none)
+        {
+            Vector3 target = transform.position;
+            if (directionY== DirectionY.up)
+            {
+                target = new Vector3(transform.position.x, transform.position.y, startY + offsetY);
+                if (Vector3.Distance(transform.position, target) < 0.1f)
+                {
+                    directionY = DirectionY.down;
+                }
+            }
+            else
+            if (directionY == DirectionY.down)
+            {
+                target = new Vector3(transform.position.x, transform.position.y, startY - offsetY);
+                if (Vector3.Distance(transform.position, target) < 0.1f)
+                {
+                    directionY = DirectionY.up;
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);           
+        }
+
+        if (directionX != DirectionX.none)
+        {
+            Vector3 target = transform.position;
+            if (directionX == DirectionX.left)
+            {
+                target = new Vector3(startX - offsetX, transform.position.y, transform.position.z);
+                if (Vector3.Distance(transform.position, target) < 0.1f)
+                {
+                    directionX = DirectionX.right;
+                }
+            }
+            else
+            if (directionX == DirectionX.right)
+            {
+                target = new Vector3(startX + offsetX, transform.position.y, transform.position.z);
+                if (Vector3.Distance(transform.position, target) < 0.1f)
+                {
+                    directionX = DirectionX.left;
+                }
+            }
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        }
+    }
+}
--- GameJam2NEW/Assets/PlayerControl.cs
+++ GameJam2NEW/Assets/PlayerControl.cs
@@ -4,162 +4,76 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float Sensitivity = 10;
-    Vector3 pos;
+   
 
-    public float h;
-    public float v;
+   
+   
+    Rigidbody rigidbody;
+    MeshRenderer renderer;
 
-    Rigidbody rb;
-    private Vector3 screenPoint;
-    private Vector3 offset;
+    public bool remote = false;
 
     void Start()
     {
 
         Cursor.lockState = CursorLockMode.Locked;
+        rigidbody = GetComponent<Rigidbody>();
+        mouseCurLocation = new Vector3(Input.mousePosition.x, 0.5f, Input.mousePosition.y);
+        mousePreviousLocation = mouseCurLocation;
+        //Cursor.visible = false;
+        renderer = GetComponent<MeshRenderer>();
     }
 
 
-    float horizontalSpeed = 0.05f;
-    float verticalSpeed = 0.05f;
 
-    void Update()
+    
+    public Vector3 mousePreviousLocation;
+    public Vector3 mouseCurLocation;
+    public Vector3 force;
+    public Vector3 objectCurrentPosition;
+    public Vector3 objectTargetPosition;
+    public float topSpeed = 5;
+
+    Color currentColor = Color.black;
+
+    void FixedUpdate()
     {
 
+        if (remote)
+        {
+            mouseCurLocation = new Vector3(Input.mousePosition.x, 0.5f, Input.mousePosition.y);
+            force = mouseCurLocation - mousePreviousLocation;
+        }
+        else
+        {
+            force = new Vector3(Input.GetAxis("Mouse X") * topSpeed, 0.5f, Input.GetAxis("Mouse Y") * topSpeed);
+        }
+        mousePreviousLocation = mouseCurLocation;
 
-        // Get the mouse delta. This is not in the range -1...1
-        float h = horizontalSpeed * Input.GetAxis("Mouse X");
-            float v = verticalSpeed * Input.GetAxis("Mouse Y");
-
-       transform.Translate(-h, 0, -v);
-      
-        //screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-
-
-
-
-
-        //Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-
-        //Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-        //Debug.Log(curScreenPoint + );
-        //transform.position = new Vector3(-curPosition.x,curPosition.y, -curPosition.z);
-        //offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
-        //   new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-    }
-
-
-    void OnMouseDrag()
-    {
+        if (rigidbody.velocity.magnitude > topSpeed)
+            force = rigidbody.velocity.normalized * topSpeed;
+        
        
+        rigidbody.velocity = new Vector3(-force.x, 0, -force.z);
+        
+        mousePreviousLocation = new Vector3(Input.mousePosition.x, 0.5f, Input.mousePosition.y);
+      
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeColor();
+        }
     }
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //    //Cursor.lockState = CursorLockMode.Locked;
-    //}
+    void ChangeColor()
+    {
+        if (currentColor == Color.black)
+            currentColor = Color.white;
+        else
+            currentColor = Color.black;
 
-    //float mDelta = 0; // Pixels. The width border at the edge in which the movement work
-    //float mSpeed = 3.0f; // Scale. Speed of the movement
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //    //if (Input.mousePosition.x >= Screen.width - mDelta)
-    //    //{
-    //    //    // Move the camera
-    //    //    transform.position += transform.right * Time.deltaTime * mSpeed;
-    //    //}
+        renderer.material.color = currentColor;
+    }
 
 
-    //    //if (Input.mousePosition.x <= 0 + mDelta)
-    //    //{
-    //    //    // Move the camera
-    //    //    transform.position -= transform.right * Time.deltaTime * mSpeed;
-    //    //}
-
-
-    //    //if (Input.mousePosition.y >= Screen.height - mDelta)
-    //    //{
-    //    //    // Move the camera
-    //    //    transform.position += transform.up * Time.deltaTime * mSpeed;
-    //    //}
-
-    //    //if (Input.mousePosition.y <= 0 + mDelta)
-    //    //{
-    //    //    // Move the camera
-    //    //    transform.position -= transform.up * Time.deltaTime * mSpeed;
-    //    //}
-
-
-    //    //Cursor.lockState = CursorLockMode.None;
-    //    //if (Cursor.lockState == CursorLockMode.Confined)
-    //    //    Cursor.lockState = CursorLockMode.Locked;
-    //    //else
-    //    //    Cursor.lockState = CursorLockMode.Confined;
-    //    //if (Input.mousePosition.x - pos.x > 0)
-    //    //{
-    //    //    h = 1;
-
-    //    //}
-    //    //else
-    //    //if (Input.mousePosition.x - pos.x < 0)
-    //    //{
-    //    //    h = -1;
-
-    //    //}
-    //    //else
-    //    //    h = 0;
-    //    //if (Input.mousePosition.y - pos.y > 0)
-    //    //{
-    //    //    v = 1;
-
-    //    //}
-    //    //else
-    //    //if (Input.mousePosition.y - pos.y < 0)
-    //    //{
-    //    //    v = -1;
-
-    //    //}
-    //    //else
-    //    //    v = 0;
-    //    h = 0;
-    //    if (Input.GetAxis("Mouse X") > 0)
-    //        h = 1;
-
-    //    if (Input.GetAxis("Mouse X") < 0)
-    //        h = -1;
-
-    //    v = 0;
-
-    //    if (Input.GetAxis("Mouse Y") > 0)
-    //        v = 1;
-
-    //    if (Input.GetAxis("Mouse Y") < 0)
-    //        v = -1;
-
-
-
-    //      Debug.Log("h - " + h + ", v - " + v);
-
-
-
-    //    transform.Translate(new Vector3((h) * Sensitivity, 0, (v) * Sensitivity));
-
-    //    //pos = Input.mousePosition;
-
-
-    //    //rb.AddForce(h * Sensitivity, 0, v * Sensitivity);
-    //    //h = Input.GetAxis("Mouse X");
-    //    //v = Input.GetAxis("Mouse Y");
-    //    //rb.AddForce(new Vector3((-h) * Sensitivity, 0, (-v) * Sensitivity));
-    //    //rb.AddForce(new Vector3((Input.mousePosition.x - pos.x) * Sensitivity, 0, (-Input.mousePosition.y + pos.y) * Sensitivity));
-
-    //}
 }
